af5da7bbce40fa7c3d4cc9316354bc9abb0723ec
mcleanka <mcleankasambala@gmail.com>
2021-06-05 23:43:49 +0200

    updated and removed defined variables

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,10 @@
 <?php
-    if (! defined('_FOLDER_PATH_')) {
-        define( '_FOLDER_PATH_', $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR);
+    if (! defined('SITE_PATH')) {
+        define( 'SITE_PATH', $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR);
     }
 
     if (! defined('__app__')) {
-        define( '__app__', _FOLDER_PATH_);
+        define( '__app__', SITE_PATH);
     }
 
     define('APP_NAME', 'PHP REST API TUTORIAL');
-    defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-    defined('INC_PATH') ? null : define('INC_PATH', __app__ . 'includes');
-    defined('CORE_PATH') ? null : define('CORE_PATH', __app__ . 'core');
