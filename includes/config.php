051841b3faf0056deb2b8886153295082afccefc
mcleanka <mcleankasambala@gmail.com>
2021-06-06 10:05:48 +0200

    updated syntax and added database config variables

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,24 @@
 <?php
-    if (! defined('SITE_PATH')) {
-        define( 'SITE_PATH', $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR);
+    if (! defined('db_name')) {
+        define( 'db_name', 'phprest');
+    }
+    
+    if (! defined('db_pass')) {
+        define( 'db_pass', 'password');
+    }
+    
+    if (! defined('db_user')) {
+        define( 'db_user', 'root');
+    }
+
+    if (! defined('app_name')) {
+        define('app_name', 'PHP REST API TUTORIAL');
+    }
+
+    if (! defined('site_path')) {
+        define( 'site_path', $_SERVER['DOCUMENT_ROOT']);
     }
 
     if (! defined('__app__')) {
-        define( '__app__', SITE_PATH);
+        define( '__app__', site_path.DIRECTORY_SEPARATOR);
     }
-
-    define('APP_NAME', 'PHP REST API TUTORIAL');
